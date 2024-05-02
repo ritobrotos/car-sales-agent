246c44619fdee60b70fc09936583d8aca293e61c
Ritobroto Seth <ritobroto@spotnana.com>
2024-05-03 01:59:33 +0530

    Updated db query composer.

--- src/sales_agent/sales_agent_db_query_composer.py
+++ src/sales_agent/sales_agent_db_query_composer.py
@@ -18,9 +18,9 @@ car_table_schema = """CREATE TABLE car (
   color VARCHAR(50) NOT NULL,             -- Color of the car
   mileage INT,                             -- Current mileage of the car
   transmission ENUM('Automatic', 'Manual'), -- Transmission type (Automatic or Manual)
-  vin VARCHAR(17) UNIQUE,                 -- Unique Vehicle Identification Number
-  price DECIMAL(10,2) DEFAULT NULL,        -- Asking or purchase price of the car
-  ownership VARCHAR(20) NOT NULL,            -- Ownership status (First, Second or Third Owner)
+  vin VARCHAR(17) UNIQUE,                    -- Unique Vehicle Identification Number
+  price INT DEFAULT NULL,                   -- Asking or purchase price of the car
+  ownership VARCHAR(20) NOT NULL,            -- Ownership status or the owner number (First Owner, Second Owner or Third Owner)
   fuel_type VARCHAR(20) NOT NULL,            -- Fuel type (Petrol, Diesel, Electric or CNG)
   insurance_valid_date DATE NOT NULL,       -- Insurance expiry date
 );"""
@@ -69,33 +69,46 @@ few_shot_rectified_query_examples = [
             FROM car
             WHERE make = 'Honda' AND model = 'Civic' AND model_year = 2023;
         """
+    },
+    {
+        "input": "Is there anything else I can help you with?",
+        "output": "NA"
     }
 ]
 
 natural_language_to_sql_query_prompt = """You are a query translator, you take human natural language as input and convert it into a \
     valid Postgres SQL query.
     
-    **Important:** When building the query, consider only the following columns from the `car` table: car_id, make, model, model_year, color, mileage, transmission, vin, price. Ignore any filters based on columns that are not present in the table (e.g., sunroof, heated_seats).
+    **Important:** When building the query, consider only the below columns from the `car` table: 
+    car_id, make, model, model_year, color, mileage, transmission, vin, price, ownership, fuel_type and insurance_valid_date. \
+    Ignore any filters based on columns that are not present in the table (e.g., sunroof, heated_seats).
     
     Prepare the query using the information provided in the user's message. If a specific filter (e.g., transmission) is not mentioned by the user, omit that condition from the query.
     
     Below is the `car` table schema:
     {car_table_schema}
     
-    Below are few examples of user messages and their corresponding SQL queries. Use these examples to convert the user message into a valid SQL query.
+    Below are few examples of user messages and their corresponding SQL queries. \
+    You can use these examples as reference to convert the user message into a valid SQL query.
 """
 
 validate_composed_sql_query_prompt = """You are an SQL query validator. You take a SQL query as input and validate the \
     SQL with the given table schema.
     If the SQL query is valid, return the query. If the SQL query is invalid, rectify the query and return.
     
-    In case of scenarios where the input is not a SQL query then return NA as response.
+    In case of scenarios where the input text cannot be rectified to a valid query, return "NA" as response.
     
     Below is the `car` table schema:
     {car_table_schema}
     
     Below are few examples of rectified SQL queries:
 """
+
+
+def log_it(logs: str):
+    with open('/Users/ritobrotoseth/Documents/workspace/python/car-sales-agent/src/sales_agent/logs/query_composer.log', 'a') as f:
+        f.write(logs)
+        f.write("================================\n\n")
 
 
 def prepare_with_few_shot_prompt_sql_query(user_message: str):
@@ -115,13 +128,14 @@ def prepare_with_few_shot_prompt_sql_query(user_message: str):
     )
 
     # Final prompt send to the LLM
-    print("************************** Final Prompt from prepare_with_few_shot_prompt_sql_query: ",
-          final_prompt_template.format_prompt(input=user_message, car_table_schema=car_table_schema).to_messages(), "\n")
+    # log_it("************************** Final Prompt from prepare_with_few_shot_prompt_sql_query: " \
+    #     + str(final_prompt_template.format_prompt(input=user_message, car_table_schema=car_table_schema).to_messages()) \
+    #     + "\n\n")
+
     output_parser = StrOutputParser()
     chain = final_prompt_template | llm | output_parser
     composed_sql_query = chain.invoke({"input": user_message, "car_table_schema": car_table_schema})
     print("Composed SQL Query: ", composed_sql_query)
-    print("**************************\n\n\n")
     return composed_sql_query
 
 
@@ -144,19 +158,19 @@ def validate_composed_sql_query(user_query: str):
             few_shot_prompt,
             ("human", "{input}"),
         ])
-    print("************************** Final Prompt from validate_composed_sql_query: ",
-          final_prompt_template.format_prompt(car_table_schema=car_table_schema, input=composed_sql_query).to_messages(),
-          "\n")
+
+    # log_it("************************** Final Prompt from validate_composed_sql_query: " \
+    #       + str(final_prompt_template.format_prompt(car_table_schema=car_table_schema, input=composed_sql_query).to_messages()) \
+    #       + "\n")
 
     output_parser = StrOutputParser()
     chain = final_prompt_template | llm | output_parser
     rectified_query = chain.invoke({"car_table_schema": car_table_schema, "input": composed_sql_query})
-    print("Rectified SQL Query: ", rectified_query)
-    print("**************************\n\n\n")
+    print("Verified SQL Query: ", rectified_query)
     return rectified_query
 
 
 if __name__ == '__main__':
-    # user_query = "I am looking for a second hand ford car with a sunroof and heated seats. Do you have any models in stock that fit that description?"
-    user_query = "Do you have any Honda cars in stock?"
-    validate_composed_sql_query(user_query)
+    # user_query_sample = "I am looking for a second hand ford car with a sunroof and heated seats. Do you have any models in stock that fit that description?"
+    user_query_sample = "Do you have any Honda cars in stock?"
+    validate_composed_sql_query(user_query_sample)
